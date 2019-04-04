0ada58b501fcc35e328a573789d0264dc799c124
Jason Chu <jchu@pivotal.io>
2019-04-04 17:55:34 -0400

    WIP: Working on in script
    Signed-off-by: Joris Melchior <joris.melchior@gmail.com>

--- assets/in.py
+++ assets/in.py
@@ -5,19 +5,34 @@ import os
 import signal
 import sys
 
+import requests
+
 
 def handler(signum, frame):
     print('Operation Timed Out', file=sys.stderr)
     exit(1)
 
 
-def process_in():
+def call_spinnaker(source):
+    return requests.get(
+        source['base_url'] + 'api/path',
+        params={'pipeline_id': source['pipeline_id'], 'resource_name': source['resource_name']}
+    ).json()
+
+
+def capture_input():
+    with sys.stdin as standard_in:
+        request = json.load(standard_in)
+
+    return request
+
+
+def process_in(directory=None):
     signal.alarm(5)
 
     try:
         try:
-            with sys.stdin as standard_in:
-                request = json.load(standard_in)
+            request = capture_input()
         except json.decoder.JSONDecodeError:
             print('No configuration provided', file=sys.stderr)
             exit(1)
@@ -38,10 +53,17 @@ def process_in():
         exit(124)
 
 
-def main():
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    if len(argv) < 2:
+        print('Usage: %s PATH' % argv[0], file=sys.stderr)
+        exit(1)
+
     signal.signal(signal.SIGALRM, handler)
 
-    process_in()
+    process_in(argv[1])
 
 
 if __name__ == '__main__':
